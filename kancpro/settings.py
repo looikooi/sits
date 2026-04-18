7a67b88ea068b754726499d0f489218f4440642a
looikooi <mardanovariet5@gmail.com>
2026-04-18 18:25:41 +0600

    fix csrf

--- kancpro/settings.py
+++ kancpro/settings.py
@@ -22,11 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = ['Cansa']  # обязательно добавь эти хосты
-
+ALLOWED_HOSTS = ['.onrender.com']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.onrender.com"
+]
 # Application definition
 
 INSTALLED_APPS = [
